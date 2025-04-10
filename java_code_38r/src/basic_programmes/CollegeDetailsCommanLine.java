ecbefa9882505ebb4441ace7660b223151f0a0ba
gvijay00 <gurugubellivijayakumar.com>
2025-04-10 23:40:53 +0530

    CollegeDetailsCommanLine

---
+++ java_code_38r/src/basic_programmes/CollegeDetailsCommanLine.java
@@ -0,0 +1,15 @@
+package basic_programmes;
+
+public class CollegeDetailsCommanLine {
+    public static void main(String[] args) {
+        System.out.println("College Name: " + args[0]);
+        System.out.println("Location: " + args[1]);
+        System.out.println("Established Year: " + args[2]);
+        System.out.println("Number of Departments: " + args[3]);
+    }
+}
+
+/*
+	Run the program with command-line arguments:
+	java CollegeDetails vignan  vishakapatnam 1998 5
+*/
