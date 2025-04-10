3fcc791cccc238c13b452b2cd0ca9d58e5689799
gvijay00 <gurugubellivijayakumar.com>
2025-04-10 23:38:58 +0530

    CollegeDetails

---
+++ java_code_38r/src/basic_programmes/CollegeDetails.java
@@ -0,0 +1,11 @@
+package basic_programmes;
+
+public class CollegeDetails {
+	
+    public static void main(String[] args) {
+        System.out.println("College Name: vignan");
+        System.out.println("Location: vishakapatnam, ");
+        System.out.println("Established Year: 1998");
+        System.out.println("Number of Departments: 5");
+    }
+}
