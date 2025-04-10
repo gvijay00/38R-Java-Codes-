95756cf545897fd5bac3672b5a20e4f70401c16b
gvijay00 <gurugubellivijayakumar.com>
2025-04-10 23:48:02 +0530

    ArithmeticOperators

---
+++ java_code_38r/src/operators/ArithmeticOperators.java
@@ -0,0 +1,25 @@
+package operators;
+
+public class ArithmeticOperators {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+
+		int add = a + b;
+		System.out.println("Addition: " + add); // 15
+
+		int minus = a - b;
+		System.out.println("Subtraction: " + minus); // 5
+
+		int mul = a * b;
+		System.out.println("Multiplication: " + mul); // 50
+
+		int div = a / b;
+		System.out.println("Division: " + div); // 2
+
+		int modulas = a % b;
+		System.out.println("Modulus: " + modulas); // 0
+
+	}
+}
