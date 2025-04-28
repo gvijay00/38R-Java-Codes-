7cea47b76c9b172f700616ca3151dc804ec348bd
gvijay00 <gurugubellivijayakumar.com>
2025-04-28 13:00:51 +0530

    If Example

---
+++ java_code_38r/src/conditional_statements/IfExample.java
@@ -0,0 +1,10 @@
+package conditional_statements;
+public class IfExample {
+    public static void main(String[] args) {
+        int number = 10;
+
+        if (number > 0) { // Condition to check if the number is positive
+            System.out.println("The number is positive.");
+        }
+    }
+}
